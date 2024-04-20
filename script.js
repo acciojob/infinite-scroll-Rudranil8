37c2083ff8e10b9ee15d4e1311a8a0a3b920d1ad
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 03:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here!
+function addItems(num) {
+  const list = document.getElementById('list');
+  for (let i = 0; i < num; i++) {
+    const item = document.createElement('li');
+    item.textContent = `Item ${list.childElementCount + 1}`;
+    list.appendChild(item);
+  }
+}
 
+addItems(10);
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    addItems(2);
+  }
+});

1243e450fb02e2b54817f849a969877ef88e193b
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 22:04:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function addItems(num) {
-  const list = document.getElementById('list');
-  for (let i = 0; i < num; i++) {
-    const item = document.createElement('li');
-    item.textContent = `Item ${list.childElementCount + 1}`;
-    list.appendChild(item);
-  }
-}
+// function addItems(num) {
+//   const list = document.getElementById('list');
+//   for (let i = 0; i < num; i++) {
+//     const item = document.createElement('li');
+//     item.textContent = `Item ${list.childElementCount + 1}`;
+//     list.appendChild(item);
+//   }
+// }
 
-addItems(10);
+// addItems(10);
+
+// window.addEventListener('scroll', () => {
+//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+//     addItems(2);
+//   }
+// });
+for (let i = 0; i < 10; i++) {
+  addItem(i);
+}
 
 window.addEventListener('scroll', () => {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    addItems(2);
+    addItem(i++);
+    addItem(i++);
   }
 });
+
+function addItem(i) {
+  const li = document.createElement('li');
+  li.textContent = 'Item ' + i;
+  document.querySelector('ul').appendChild(li);
+}

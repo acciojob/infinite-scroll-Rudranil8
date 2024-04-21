ada69d8d09549757456cf24728d1997231aa5983
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 22:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// function addItems(num) {
+//   const list = document.getElementById('list');
+//   for (let i = 0; i < num; i++) {
+//     const item = document.createElement('li');
+//     item.textContent = `Item ${list.childElementCount + 1}`;
+//     list.appendChild(item);
+//   }
+// }
+
+// addItems(10);
+
+// window.addEventListener('scroll', () => {
+//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight -10) {
+//     addItems(2);
+//   }
+// });
 function addItems(num) {
-  const list = document.getElementById('list');
+  const list = document.getElementById('infi-list'); // changed from 'list' to 'infi-list'
   for (let i = 0; i < num; i++) {
     const item = document.createElement('li');
     item.textContent = `Item ${list.childElementCount + 1}`;
@@ -10,7 +26,7 @@ function addItems(num) {
 addItems(10);
 
 window.addEventListener('scroll', () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight -10) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
     addItems(2);
   }
 });

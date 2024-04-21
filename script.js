32f66857d15bce8ab82ae12ec30f65ecd2ac6c62
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 22:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-// function addItems(num) {
-//   const list = document.getElementById('list');
-//   for (let i = 0; i < num; i++) {
-//     const item = document.createElement('li');
-//     item.textContent = `Item ${list.childElementCount + 1}`;
-//     list.appendChild(item);
-//   }
-// }
-
-// addItems(10);
-
-// window.addEventListener('scroll', () => {
-//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-//     addItems(2);
-//   }
-// });
-for (let i = 0; i < 10; i++) {
-  addItem(i);
+function addItems(num) {
+  const list = document.getElementById('list');
+  for (let i = 0; i < num; i++) {
+    const item = document.createElement('li');
+    item.textContent = `Item ${list.childElementCount + 1}`;
+    list.appendChild(item);
+  }
 }
+
+addItems(10);
 
 window.addEventListener('scroll', () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    addItem(i++);
-    addItem(i++);
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight -10) {
+    addItems(2);
   }
 });
-
-function addItem(i) {
-  const li = document.createElement('li');
-  li.textContent = 'Item ' + i;
-  document.querySelector('ul').appendChild(li);
-}
